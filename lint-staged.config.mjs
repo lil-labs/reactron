c6408960902be023f44a9a3ae1570be62cdd847a
sho-yamane <sns.yamane@gmail.com>
2025-01-11 14:31:44 +0900

    Add lint-staged configuration for ESLint and Prettier, update package.json for husky installation, clean up pnpm-lock.yaml by removing unused dependencies, and adjust tsconfig.json to exclude test files.

---
+++ lint-staged.config.mjs
@@ -0,0 +1,7 @@
+export default {
+  "**/*.(ts|tsx|js)": (filenames) => [
+    `npx eslint --fix ${filenames.join(" ")}`,
+    `npx prettier --write ${filenames.join(" ")}`,
+  ],
+  "**/*.(md|json)": (filenames) => `npx prettier --write ${filenames.join(" ")}`,
+};

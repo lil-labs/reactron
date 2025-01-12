d2db5622e0d5523e1b187671fed523551582c2ee
Shiono Yoshihide <shiono.yoshihide@gmail.com>
2025-01-13 00:34:20 +0900

    debug

--- scripts/release-latest.ts
+++ scripts/release-latest.ts
@@ -2,7 +2,7 @@ import { $ } from 'execa'
 
 const $$ = $({
   stdio: 'inherit',
-  env: { NODE_ENV: 'production', HUSKY_SKIP_HOOKS: '1' },
+  env: { NODE_ENV: 'production', LEFTHOOK: '0' },
 })
 
 await $$`lerna publish`
